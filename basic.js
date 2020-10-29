fdc3d92fa6970d33e0f89a757680bc14cbcda28a
AlexKuo <alexmax28@gmail.com>
2020-10-30 00:28:49 +0800

    FQbasic add

---
+++ basic.js
@@ -0,0 +1,189 @@
+$(document).ready(function(){
+    // 3性別懷孕判斷
+    $("input[name=sex]").on('click', function() { 
+        var sex = $('input[name=sex]:checked').val();
+        if(sex =='boy'){
+            $('input[name = pregnant]').attr('disabled', true);
+        }
+        else if(sex=='girl'){
+            $('input[name = pregnant]').removeAttr('disabled');
+        }     
+
+    // 3性別哺乳判斷
+        if(sex=='boy'){
+            $('input[name = nursing]').attr('disabled', true)
+        }
+        else if(sex=='girl'){
+            $('input[name = nursing]').removeAttr('disabled');
+        } 
+     } );
+
+    //  8教育程度判斷
+     $("select[name=education_level]").on('click', function() { 
+     var selectVal = $('[name=education_level]').val()
+     if(selectVal=='education_other'){
+         $('.other').removeAttr('disabled');
+     }
+     else{
+         $('.other').attr('disabled',true);
+     }
+    } );
+
+    // 9素食判斷
+    $("select[name=vegetarian]").on('click', function() { 
+        var selectVal = $('[name=vegetarian]').val()
+        if(selectVal=='vegetarian_other'){
+            $('.vegetarian_other').removeAttr('disabled');
+        }
+        else{
+            $('.vegetarian_other').attr('disabled',true);
+        }
+       } );
+
+    // 13判斷牙齒痛
+    $("select[name=mouth_tooth]").on('click', function() { 
+        var selectVal = $('[name=mouth_tooth]').val()
+        if(selectVal=='mouth_tooth7'){
+            $('.mouth_tooth7').removeAttr('disabled');
+        }
+        else{
+            $('.mouth_tooth7').attr('disabled',true);
+        }
+       } );
+
+    //    驗證
+    $(function(){
+        $('#form').validate({
+        /* 常用檢測屬性
+       required:必填
+       noSpace:空白
+       minlength:最小長度
+       maxlength:最大長度
+       email:信箱格式
+       number:數字格式
+       url:網址格式https://www.minwt.com
+       */
+       onkeyup: function(element, event) {
+         //去除左側空白
+         var value = this.elementValue(element).replace(/^\s+/g, "");
+         $(element).val(value);
+        },
+
+        rules: {
+          name: {
+            required: true
+          },
+          verification_code:{
+            required: true,
+            minlength: 8,
+            number: true
+          },
+          sex:{
+            required: true
+          },
+          weight:{
+            required: true,
+            range: [1, 300]
+          },
+          height:{
+            required: true,
+            range: [100, 300]
+          },
+          activity:{
+            required: true,
+          },
+          education_level:{
+            required: true,
+          },
+          vegetarian:{
+            required: true,
+          },
+          mouth_tooth:{
+            required: true,
+          },
+          address: 'required',
+          url:{
+            url: true
+          },
+          email: {
+            required: true,
+            email: true
+          }
+        },
+
+
+
+        messages: {
+          name: {
+            required:'必填'
+          },
+          verification_code: {
+            required:'必填',
+            minlength:'不得少於8位',
+            number:'編號需為數字'
+          },
+          sex:{
+            required: '必填'
+          },
+
+          weight:{
+            required: '必填',
+            range:'1公斤至300公斤之間'
+          },
+          height:{
+            required: '必填',
+            range:'100公分至300公分之間'
+          },
+          activity:{
+            required: '必填',
+          },
+          education_level:{
+            required: '必填',
+          },
+          vegetarian:{
+            required: '必填',
+          },
+          mouth_tooth:'必填',
+       
+   
+
+
+        //   address: '必填',
+        //   email: {
+        //     required:'必填',
+        //     email:'Email格式不正確'
+        //   },
+        //   url:'網址格式不正確'
+        },
+        submitHandler: function(form) {
+          form.submit();
+        }
+  });
+
+});
+})
+jQuery.extend(jQuery.validator.defaults,
+  {
+      errorPlacement: function (error, element) {
+          if (element.is(':radio') || element.is(':checkbox')) {
+              var eid = element.attr('name');
+              $('input[name=' + eid + ']:last').next().after(error);
+          }
+          else {
+              error.insertAfter(element);
+          }
+      }
+  });
+
+
+
+//生日
+for(var i=0;i<109;i++){
+    $(".birth_year").append('<option>'+[i+1]+'</option>');
+};
+for(var i=0;i<12;i++){
+  $(".birth_month").append('<option>'+[i+1]+'</option>');
+};
+for(var i=0;i<31;i++){
+  $(".birth_date").append('<option>'+[i+1]+'</option>');
+};
